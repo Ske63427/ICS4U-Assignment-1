521493ad0ef2ade72a83598171c9403bc3286c4e
email <skelecrae63427@gmail.com>
2024-09-12 11:27:04 -0400

    added semicolon on line 3 in Tres.java

--- Tres.java
+++ Tres.java
@@ -1,5 +1,5 @@
 public class main{
     public static void main(String[] args){
-        System.out.println("This is the third file commited")
+        System.out.println("This is the third file commited");
     }
 }
